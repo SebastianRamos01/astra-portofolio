0644060ce82e651c8c6fc784fb77899bd84d30ad
SebastianRamos01 <sebasr.sr@hotmail.com>
2025-04-13 00:23:09 -0300

    first commit

---
+++ src/data/Works.js
@@ -0,0 +1,24 @@
+export const works = [
+    {
+        id: 1,
+        title: 'Graphic Studio',
+        images: [
+            'Pexels Photo by Ali Pazani.png',
+            'Pexels Photo by Amar  Preciado.png',
+            'Pexels Photo by Bobby Dimas.png',
+            'Pexels Photo by Collins Mike.png',
+            'Pexels Photo by Eunhyuk Ahn.png',
+        ]
+    },
+    {
+        id: 2,
+        title: 'Freelancing',
+        images: [
+            'Pexels Photo by Jonathan Santiago.png',
+            'Pexels Photo by Nikita Pishchugin.png',
+            'Pexels Photo by RD.png',
+            'Pexels Photo by Ulkar Batista.png',
+            'Pexels Photo by Милана Отчесова.png',
+        ]
+    },
+]
